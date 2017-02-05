f71def6a45c43f5b03af5c6e1f8ea81c675c3606
Matt Mulit <mattmulit@hotmail.com>
2017-02-05 14:56:42 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('#submitSearch').submit(function(event) {
+        event.preventDefault();
+        var searchTerm = $('#text').val();
+        getRequest(searchTerm);
+    });
+});
+
+function getRequest(searchTerm) {
+    url = 'https://www.googleapis.com/youtube/v3/search';
+    var params = {
+        part: 'snippet',
+        key: 'AIzaSyCMPZP6_oUwbGrCcNLvZi9XpmH86bk6NPk',
+        q: searchTerm
+    };
+
+    $.getJSON(url, params, function(searchTerm) {
+        showResults(searchTerm);
+    });
+}
+
+var searchResult = function(index, value) {
+    this.index = index;
+    this.title = value.snippet.title;
+    this.address = 'https://www.youtube.com/watch?v=' + value.id.videoId;
+    this.thumbnail = value.snippet.thumbnails.default.url;
+}
+
+function showResults(results) {
+    var html = "";
+    var entries = results.items;
+    $.each(entries, function(index, value) {
+        var video = new searchResult(index, value);
+        html += '<p><a href="' + video.address + '">' + video.title + '</a></p>';
+        html += '<a href="' + video.address + '"><img src="' + video.thumbnail + '"/></a>';
+    });
+
+    $('#results').html(html);
+}
